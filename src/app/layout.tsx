8f74791d67c86e7a781063a4c5fe00d984b38ba7
Naidu-4444 <22pa1a0539@vishnu.edu.in>
2025-09-10 23:30:07 +0530

    feat: Add main chat interface components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
+// src/app/layout.tsx
+
 import type { Metadata } from "next";
 import { Bricolage_Grotesque } from "next/font/google";
+import BackgroundImage from "@/components/BackgroundImage";
 import Providers from "./Providers";
 import "./globals.css";
-import BackgroundImage from "@/components/BackgroundImage";
 
 const bricolage_grotesque_init = Bricolage_Grotesque({
   subsets: ["latin"],
@@ -22,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${bricolage_grotesque_init.className} antialiased h-screen dark:bg-black bg-white`}
+        className={`${bricolage_grotesque_init.className} antialiased h-screen`}
       >
         <Providers>
           <BackgroundImage />
---
+++ src/components/Appbar.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import ThemeToggleButton from "./ThemeToggleButton";
+
+export default function Appbar() {
+  return (
+    <header className="w-full p-1 mt-3 max-sm:mt-7 rounded-lg px-8 max-sm:px-4">
+      <nav className="w-full flex items-center justify-between">
+        <h1>
+          <Link
+            href="/"
+            className={`font-extrabold text-xl max-sm:text-lg tracking-tight`}
+          >
+            Lumina
+          </Link>
+        </h1>
+        <ThemeToggleButton />
+      </nav>
+    </header>
+  );
+}
